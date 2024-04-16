b4d8df929391469749e3cccc673bc7013da006b0
Scott Dohei <dohei@dohei>
2024-04-16 10:26:49 -0600

    try without widget

--- woo-naiop.php
+++ woo-naiop.php
@@ -5,7 +5,7 @@
  * Description:         WooCommerce customizations for NAIOP
  * Author:              Scott Dohei
  * GitHub Plugin URI:   https://github.com/sdohei/dohei-woo-naiop
- * Version:             1.3
+ * Version:             1.4
  */
 
 add_action('woocommerce_after_order_notes', 'custom_field');
@@ -13,18 +13,6 @@ add_action('woocommerce_after_order_notes', 'custom_field');
 function custom_field($checkout) {
     echo '<div id="naiop_checkout_diet"><h3>' . __('Please Provide The Custom Data') . '</h3>';
 
-    woocommerce_form_field('naiop_checkout_diet',
-        array(
-            'type' => 'text',
-            'required' => 'true',
-            'class' => array(
-                'my-field-class form-row-wide'
-            ),
-            'label' => __('Custom Field'),
-            'placeholder' => __('Enter Custom Data'),
-        ),
-        $checkout->get_value('naiop_checkout_diet')
-    );
 
     echo '</div>';
 }
