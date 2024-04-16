7946f998edc094b2156c5f639d4a70377e03e6cd
Scott Dohei <dohei@dohei>
2024-04-16 10:15:47 -0600

    add checkout field

--- woo-naiop.php
+++ woo-naiop.php
@@ -1,15 +1,30 @@
 <?php
 
 /**
- * Plugin Name: WooCommerce customizations for NAIOP
- * GitHub Plugin URI: https://github.com/sdohei/dohei-woo-naiop
- * Version:     1.1
+ * Plugin Name:         WooCommerce NAIOP
+ * Decription:          WooCommerce customizations for NAIOP
+ * Author:              Scott Dohei
+ * GitHub Plugin URI:   https://github.com/sdohei/dohei-woo-naiop
+ * Version:             1.2
  */
 
- function display_page() {
-    echo 'hello world';
-  }
-  function hello_admin_menu() {
-    add_menu_page('hello', 'hello', 'manage_options', 'hello-world', 'display_page');
-  }
-  add_action('admin_menu', 'hello_admin_menu');
+add_action('woocommerce_after_order_notes', 'custom_field');
+
+function custom_field($checkout) {
+    echo '<div id="naiop_checkout_diet"><h3>' . __('Please Provide The Custom Data') . '</h3>';
+
+    woocommerce_form_field('naiop_checkout_diet',
+        array(
+            'type' => 'text',
+            'required' => 'true',
+            'class' => array(
+                'my-field-class form-row-wide'
+            ),
+            'label' => __('Custom Field'),
+            'placeholder' => __('Enter Custom Data'),
+        ),
+        $checkout->get_value('naiop_checkout_diet')
+    );
+
+    echo '</div>';
+}
