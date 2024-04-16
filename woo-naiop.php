9721135c0784efd0bce424b15c57018c30e5bbbb
Scott Dohei <dohei@dohei>
2024-04-16 09:54:33 -0600

    menu test

--- woo-naiop.php
+++ woo-naiop.php
@@ -3,6 +3,13 @@
 /**
  * Plugin Name: WooCommerce customizations for NAIOP
  * GitHub Plugin URI: https://github.com/sdohei/dohei-woo-naiop
- * Version:     1.0
+ * Version:     1.1
  */
 
+ function display_page() {
+    echo 'hello world';
+  }
+  function hello_admin_menu() {
+    add_menu_page('hello', 'hello', 'manage_options', 'hello-world', 'display_page');
+  }
+  add_action('admin_menu', 'hello_admin_menu');
